c1811a88445dfa050ac34e56fe244f36b3eadf99
itadmin <itadmin@class.local>
2019-11-23 13:47:17 -0800

    update

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 //array for answers
 const array = [];
