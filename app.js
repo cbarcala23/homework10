4c223c6fed0c80026444204f424b61a4ac0f8958
itadmin <itadmin@class.hsd1.wa.comcast.net>
2019-11-25 23:46:51 -0800

    updates

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("./generateHTML");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-//array for answers
-const array = [];
 
 //Questions
 const questions = [
@@ -76,17 +78,101 @@ function init() {
     inquirer
         .prompt(questions)
         .then(function (input) {
-            array.push(input);
-            // console.log(input);
-            console.log(array);
+            if (input.title === "Manager") {
+                var object = new Manager(input.name, input.id, input.email, input.officeNumber);
+                var html = managerCard(object);
+                fs.appendFile('./index.html', html, function (err) {
+                    if (err) throw err;
+                });
+            } else if (input.title === "Engineer") {
+                var object = new Engineer(input.name, input.id, input.email, input.github);
+                var html = engineerCard(object);
+                fs.appendFile('./index.html', html, function (err) {
+                    if (err) throw err;
+                });
+            } else if (input.title === "Intern") {
+                var object = new Intern(input.name, input.id, input.email, input.school);
+                var html = internCard(object);
+                fs.appendFile('./index.html', html, function (err) {
+                    if (err) throw err;
+                });
+            }
             if (input.repeat === 'Yes') {
                 init();
-            } else {
-                return;
             }
+            return;
         })
 };
 
+function managerCard(managerObj) {
+    var name = managerObj.getName();
+    var id = managerObj.getID();
+    var email = managerObj.getEmail();
+    var officeNumber = managerObj.getOfficeNumber();
+    var role = managerObj.getRole();
+
+    var html = `<div class="card">
+    <h4 class="card-header" id="name">`+ name + `</h4>
+    <h4 class="card-header" id="name">`+ `<i class="fas fa-gavel"></i>`+`"  "` + role + `</h4>
+    <div class="card-body">
+        <p class="card-text"><b></b><span id="icon"></span> </p>
+        <p class="card-text"><b>ID : `+ id + `</b><span id="temp"></span> </p>
+        <p class="card-text"><b>Email : `+ email + `</b><span id="humidity"></span></p>
+        <p class="card-text"><b>Office # : `+ officeNumber + `</b><span id="uv"> </span></p>
+    </div>
+    </div>`;
+    //console.log("manager"+html);
+    return html;
+}
+
+function engineerCard(engineerObj) {
+    var name = engineerObj.getName();
+    var id = engineerObj.getID();
+    var email = engineerObj.getEmail();
+    var github = engineerObj.getGithub();
+    var role = engineerObj.getRole();
+
+    var html = `<div class="card">
+    <h4 class="card-header" id="name">`+ name + `</h4>
+    <h4 class="card-header" id="name">`+ `<i class="fas fa-wrench"></i>`+`"  "` + role + `</h4>
+    <div class="card-body">
+        <p class="card-text"><b></b><span id="icon"></span> </p>
+        <p class="card-text"><b>ID : `+ id + `</b><span id="temp"></span> </p>
+        <p class="card-text"><b>Email : `+ email + `</b><span id="humidity"></span></p>
+        <p class="card-text"><b>Github : `+ github + `</b><span id="uv"> </span></p>
+    </div>
+    </div>`;
+    //console.log("manager"+html);
+    return html;
+}
+
+function internCard(internObj) {
+    var name = internObj.getName();
+    var id = internObj.getID();
+    var email = internObj.getEmail();
+    var school = internObj.getSchool();
+    var role = internObj.getRole();
+
+    var html = `<div class="card">
+    <h4 class="card-header" id="name">`+ name + `</h4>
+    <h4 class="card-header" id="name">`+ `<i class="fas fa-user-graduate"></i>`+`"  "` + role + `</h4>
+    <div class="card-body">
+        <p class="card-text"><b></b><span id="icon"></span> </p>
+        <p class="card-text"><b>ID : `+ id + `</b><span id="temp"></span> </p>
+        <p class="card-text"><b>Email : `+ email + `</b><span id="humidity"></span></p>
+        <p class="card-text"><b>School : `+ school + `</b><span id="uv"> </span></p>
+    </div>
+    </div>`;
+    //console.log("manager"+html);
+    return html;
+}
+
+// Writes the initial HTML to the index.html file
+function writeToFile() {
+    const html = generateHTML();
+    writeFileAsync("index.html", html);
+};
 
 // Start Questions
+writeToFile();
 init();
